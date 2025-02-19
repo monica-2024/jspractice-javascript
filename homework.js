bddc9f5e95e3248be5df2fe7906ce50fc826c99d
monica-2024 <monica.baum24@gmail.com>
2025-02-19 17:19:02 -0500

    js homework 2/19/2025

---
+++ homework.js
@@ -0,0 +1,34 @@
+//2/19/2025
+// 1️⃣ Printing Strings
+//    Print "Good morning, everyone!" to the console.
+console.log("Good morning", "everyone!");
+//    Print your full name to the console.
+console.log("My full name is", "Monica", "Lee");
+//    Print three of your favorite foods, separated by commas.
+console.log(
+  "My three favorite food are",
+  "김밥",
+  "육회",
+  "그리고",
+  "남이 해준 정성스러운 음식입니다."
+);
+
+// 2️⃣ Printing Numbers
+// Print the number 2025 to the console.
+console.log("This is year is", 2025);
+// Print a floating-point number (e.g., 3.14159).
+console.log(3.14159, "is a floating-point number");
+// Print your age to the console
+console.log("I wish I were 29");
+
+// 3️⃣ Mixing Strings & Numbers
+//    Print "My name is [your name] and I am [your age] years old."
+console.log("This assignment has been done with", "1", "and", "2");
+//    Print "The current year is 2025, and next year will be 2026." using numbers inside console.log().
+console.log(
+  "The current year is",
+  "2025",
+  "and next year will be",
+  "2026",
+  "which will be better than current year."
+);
